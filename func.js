7241d68298823b1b40ab6c996652236f1c42fd5d
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2024-03-06 06:50:16 -0800

    africaplan website

---
+++ func.js
@@ -0,0 +1,9 @@
+// const if
+// const hostStudents = ["Ezekiel", "Theresa", "Kosi", "Koach"]
+// function myC
+// console.log
+
+
+let fortune = "yu"
+let newfortune = fortune.padStart("4", "k0")
+console.log(newfortune);
